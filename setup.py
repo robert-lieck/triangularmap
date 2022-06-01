22eb54f447143e1a3b8536b5fd49be88e08e0a06
Robert Lieck <robert.lieck@durham.ac.uk>
2022-06-01 15:27:12 +0100

    relax Python version requirement to 3.8

--- setup.py
+++ setup.py
@@ -23,6 +23,6 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.10",
+    python_requires=">=3.8",
 )
 
